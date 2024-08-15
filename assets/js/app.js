72f1be0f34cdc1addcc3d54fbcf182e4a23617af
JeppeFH <jeppefinch@hotmail.dk>
2024-08-15 10:22:43 +0200

    update html css js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,16 @@
-/*Navigation*/
-let burgerMenuOpen = document.querySelector(".burgerMenuOpen");
-let burgerMenuClose = document.querySelector(".burgerMenuClose");
-let navLinks = document.querySelector(".nav-links");
-/*Let = En literal der bruges til at deklarere en variabel.*/
-/*document = kobler det til ens html */
-/*querySelector =  Søger efter elementer i dokumentet, 
-der har class eller id*/
+/*To Do Liste*/
 
-burgerMenuOpen.addEventListener("click", () => {
-  /*Fortæller at der skal være en Click event listener på array'en*/
-  navLinks.classList.add("active");
-  burgerMenuOpen.classList.add("active");
-  burgerMenuClose.classList.add("active");
-  /*Laver en classList på arrays for at manipulere class samtidig 
-  en add("active") på for så at kunne manipulere class i css. */
-});
+/* Variabler */
+const toDoContainer = document.querySelector(".todo-container");
 
-burgerMenuClose.addEventListener("click", () => {
-  navLinks.classList.remove("active");
-  burgerMenuOpen.classList.remove("active");
-  burgerMenuClose.classList.remove("active");
-  /*Laver et click event på burgermenuclose som remover active class ved 
-  click. */
+/* Array med list items - opgaver på to-do listen */
+let todoArray = [
+  "Gå tur med hunden",
+  "Tømme opvaskeren",
+  "Slå græs",
+  "Støvsuge",
+];
+
+todoArray.forEach((item) => {
+  toDoContainer.innerHTML += `<p>${item}</p>`;
 });
