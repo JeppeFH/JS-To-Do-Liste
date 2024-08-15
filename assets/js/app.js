070eced8322e1c9c4469849aaca839becfce253b
JeppeFH <jeppefinch@hotmail.dk>
2024-08-15 11:31:58 +0200

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,15 +11,34 @@ let todoArray = [
   "Støvsuge",
 ];
 
-/* Udskrive array med forEach funktion */
-todoArray.forEach((item) => {
-  toDoContainer.innerHTML += `<p class="list-item" >${item}</p>`;
-});
+/* Udskrive to do liste (array) i browseren */
+function renderToDoList() {
+  /* nulstiller array så den ikke tilføjer dem hver gang man "pusher" btn */
+  toDoContainer.innerHTML = "";
 
+  todoArray.forEach((item) => {
+    toDoContainer.innerHTML += `<p class="list-item" >${item}</p>`;
+  });
+}
+/*  */
+renderToDoList();
+
+/* Tjek af på listen */
 const listItem = document.querySelectorAll(".list-item");
 
 listItem.forEach((item) => {
   item.addEventListener("click", (event) => {
     event.target.classList.toggle("done");
   });
+});
+
+/* Tilføj nyt punkt på listen */
+const input = document.querySelector(".input");
+const btnAddItem = document.querySelector(".btn-add-item");
+
+btnAddItem.addEventListener("click", () => {
+  const newItem = input.value;
+
+  todoArray.push(newItem);
+  renderToDoList();
 });
